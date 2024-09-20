6c785d672c7de733fa56b08ad8d55a4ed27d632c
Теренков Кирилл <terenkovkirill@yandex.ru>
2024-09-21 02:55:15 +0300

    Simple good version

--- Onegin4.cpp
+++ Onegin4.cpp
@@ -2,106 +2,130 @@
 #include <stdio.h>
 #include <math.h>
 #include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 
 enum Direct {
-    SIZE_Y = 18,
     SIZE_X = 90,
     RIGHT_LOCATION = 1,
     INVALID_LOCATION = 2,
+    BEGIN_STR = 1000000,
+    SIMPLE_ELEM = - 1000000,
 };
 
-void BubbleSort(char** dinamic_text);
-void ChangePlaceMaxLine(char** dinamic_text, int limiter);
+void BubbleSort(char** dinamic_text, int count_str);
+void ChangePlaceMaxLine(char** dinamic_text, int limiter, int count_str);
 int CompareLetters(int dinamic_text_i_j, int dinamic_text_i1_j);
-void PrintText(char** dinamic_text);
+void PrintText(char** dinamic_text, int count_str);
 
 
 int main()
 {
-    char text[SIZE_Y][SIZE_X] =    {"My uncle used to be so sound",             //0
-                                    "But after falling gravely ill",            //1
-                                    "Is craving homage, and has found",         //2
-                                    "That could not make a better deal.",       //3
-                                    "It is for others a good lesson,",          //4
-                                    "But what a boredom is the session:",       //5
-                                    "To watch a sick man day and night",        //6
-                                    "And do not make a step aside!",            //7
-                                    "What mean deceitfulness it is:",           //8
-                                    "To feign compassion, dose the drugs,",     //9
-                                    "To set and fluff the pillows, thus,",      //10
-                                    "To entertain a half deceased,",            //11
-                                    "To show fake sympathy, then, sigh",        //12
-                                    "And think: Oh gosh, when will you die!",   //13
-                                    "It was what a young rake expected,",       //14
-                                    "When dusting post-chaise speeded him,",    //15
-                                    "By Zeus’s supreme will elected",           //16
-                                    "The only heir of all his kin."};           //17
+    FILE *text_file = fopen("Onegin_text.txt", "rb");
+    assert(text_file != NULL);
 
+    fseek(text_file, 0, SEEK_END);                //вычисляем длину файла
+    long file_len = ftell(text_file);
+    fseek(text_file, 0, SEEK_SET);
 
-    char** dinamic_text = 0;
-    dinamic_text = (char **)calloc(SIZE_X * SIZE_Y, sizeof(char));    //создаём и заполняем динамический массив
+    char* buffer = (char *)calloc(file_len, sizeof(char));
+    fread(buffer, sizeof(char), file_len, text_file);           //считываем файл в Buffer
+    fclose(text_file);
 
-    for (int i = 0; i < SIZE_Y; i++)
+    int count_str = 0;                          //считаем кол-во строк (\n)
+    for(int i = 0; i < file_len - 1; i++)
     {
-        int size_line = sizeof(text[i]) / sizeof(text[i][0]);
-
-        dinamic_text[i] = (char *)calloc(size_line, sizeof(char));
-        assert(dinamic_text[i] != NULL);
-
-        for (int j = 0; j < size_line; j++)
+        if (buffer[i] == '\n')
         {
-            dinamic_text[i][j] = text[i][j];
+            count_str++;
         }
     }
 
-    PrintText((char **)dinamic_text);
 
-    printf("\n \n");
+    char** dinamic_text = 0;
+    dinamic_text = (char **)calloc(count_str, sizeof(char*));
+    assert(dinamic_text != NULL);
 
-    BubbleSort((char **)dinamic_text);
+    *dinamic_text = buffer;
 
-    PrintText((char **)dinamic_text);
+    int num_ptr = 0;
+    for (int i = 0; i < file_len - 1; i++)
+    {
+        assert(0 <= i && i < file_len);
+        assert(0 <= i + 1 && i + 1 < file_len);
+
+        if (buffer[i] == '\r')
+        {
+            buffer[i] = '\0';
+        }
+
+        if (buffer[i] == '\n')
+        {
+            buffer[i] = '\0';
+            num_ptr++;
+            *(dinamic_text + num_ptr) = buffer + i + 1;
+        }
+    }
+
+
+    PrintText((char **)dinamic_text, count_str);
+
+    printf("\n\n\n\n\n\n\n");
+
+    BubbleSort((char **)dinamic_text, count_str);
+    //qsort(dinamic_text, count_str, sizeof(char *), strcmp);
+
+    PrintText((char **)dinamic_text, count_str);
+
 
     return 0;
 }
 
 
 
-void BubbleSort(char** dinamic_text)
+void BubbleSort(char** dinamic_text, int count_str)
 {
     assert(dinamic_text != NULL);
 
-    int limiter = SIZE_Y - 1;
+    int limiter = count_str - 1;
     while (limiter > 0)      //SIZE_Y - 1 раз отправляем max строчку в конец
     {
-        ChangePlaceMaxLine(dinamic_text, limiter);
+        ChangePlaceMaxLine(dinamic_text, limiter, count_str);
         limiter--;
     }
 }
 
 
 
-void ChangePlaceMaxLine(char** dinamic_text, int limiter)
+void ChangePlaceMaxLine(char** dinamic_text, int limiter, int count_str)
 {
     assert(dinamic_text != NULL);
     char* temp = 0;
 
     for (int i = 0; i < limiter; i++)
     {
-        assert(0 <= i && i< SIZE_Y);          //нужны ли эти assert?
-        assert(0 <= i + 1 && i + 1 < SIZE_Y);
+        assert(0 <= i && i < count_str);          //нужны ли эти assert?
+        assert(0 <= i + 1 && i + 1 < count_str);
+
+        int space1 = 0, space2 = 0;
         for (int j = 0; j < SIZE_X; j++)
         {
-            if (CompareLetters(dinamic_text[i][j], dinamic_text[i+1][j]) == RIGHT_LOCATION)
+            space1 = space1 + isspace(dinamic_text[i][j + space1]);
+            space2 = space2 + isspace(dinamic_text[i + 1][j + space2]);
+
+            if (CompareLetters(dinamic_text[i][j + space1], dinamic_text[i + 1][j + space2]) == RIGHT_LOCATION)
             {
+                //printf("left: %s, right: %s, %d \n", dinamic_text[i], dinamic_text[i+1], j);
                 break;
             }
 
-            if ((CompareLetters(dinamic_text[i][j], dinamic_text[i+1][j])) == INVALID_LOCATION)     //меняет индексы двух отличающихся строк местами
+            if ((CompareLetters(dinamic_text[i][j + space1], dinamic_text[i + 1][j + space2])) == INVALID_LOCATION)     //меняет индексы двух отличающихся строк местами
             {
                 temp =  *(dinamic_text + i);
                 *(dinamic_text + i) = *(dinamic_text + i + 1);
                 *(dinamic_text + i + 1) = temp;
+                //printf("left_old: %s, right_old: %s, %d \n", dinamic_text[i], dinamic_text[i + 1], j);
+                //printf("left_new: %s, right_new: %s, %d \n", dinamic_text[i + 1], dinamic_text[i], j);
                 break;
             }
         }
@@ -111,48 +135,46 @@ void ChangePlaceMaxLine(char** dinamic_text, int limiter)
 
 
 
-int CompareLetters(int dinamic_text_i_j, int dinamic_text_i1_j)
+int CompareLetters(int dinamic_text_i_j1, int dinamic_text_i1_j2)
 {
-    if (isalnum(dinamic_text_i_j) && isalnum(dinamic_text_i1_j))
+    if (tolower(dinamic_text_i_j1) < tolower(dinamic_text_i1_j2))
     {
-        if (tolower(dinamic_text_i_j) < tolower(dinamic_text_i1_j))
-        {
-            return RIGHT_LOCATION;
-        }
+        return RIGHT_LOCATION;
+    }
 
-        else if (tolower(dinamic_text_i_j) > tolower(dinamic_text_i1_j))
-        {
-            return INVALID_LOCATION;
-        }
-
-        else
-        {
-            return 0;
-        }
+    else if (tolower(dinamic_text_i_j1) > tolower(dinamic_text_i1_j2))
+    {
+        return INVALID_LOCATION;
     }
 
     else
     {
         return 0;
     }
+
 }
 
 
-void PrintText(char** dinamic_text)
+void PrintText(char** dinamic_text, int count_str)
 {
     assert(dinamic_text != NULL);
 
-    for (int i = 0; i < SIZE_Y; i++)
+    for (int i = 0; i < count_str; i++)
     {
-        for (int j = 0; j < SIZE_X; j++)
-        {
-            printf("%c", dinamic_text[i][j]);
-        }
+        printf("%s", dinamic_text[i]);
         printf("\n");
     }
 }
 
+/*
+char** dinamic_text = (char **)calloc(...)    //указатель на массив указателей
+dinamic_text[1] := *(dinamic_text + 1)        //указатель на первую строку в массиве
+dinamic_text[1][2] := *(*(dinamic_text + 1) + 2)
+*/
 
 
+//правильно ли я записал flag?
+// все ли assert я поставил
 
 //проблема с isalnum (невозможно пропустить пробел в одной строке и не пропустить символ в другой)
+//todo: разность ASCII-кодов букв вместо их сравнения
